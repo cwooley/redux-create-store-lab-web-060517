edec351b29471d9d31e1b82c02bd218bb19775a1
Charles Wooley <cwooley89@gmail.com>
2017-08-24 20:40:20 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,28 @@
+import candyReducer from './reducers/candyReducer'
+
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function getState(){
+    return state;
+  }
+
+  function dispatch(action){
+    state = reducer(state,action)
+    render()
+  }
+
+  dispatch({ type: '@@INIT' });
+
+  return {
+    getState,
+    dispatch
+  }
 }
+
+
+
+
 
 function render() {
   const container = document.getElementById('container');
